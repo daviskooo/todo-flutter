f2b6a1a09df6720b0b85b4fbd76fe9c284bbf366
daviskooo <daviferreiradelima.contato@gmail.com>
2020-01-22 01:25:39 -0300

    Adicionando método para envio de dados por serialização para JSON.

--- todo/lib/models/item.dart
+++ todo/lib/models/item.dart
@@ -8,4 +8,11 @@ class Item {
     title = json['title'];
     done = json['done'];
   }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+    return data;
+  }
 }
