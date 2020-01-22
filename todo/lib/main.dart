ecafdb2c5abb33c6f07feb36fac236e17de1c80e
daviskooo <daviferreiradelima.contato@gmail.com>
2020-01-22 00:03:59 -0300

    Iniciando a estrutura básica da aplicação. Sem componentes visuais.

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,4 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() => runApp(App());
 
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
