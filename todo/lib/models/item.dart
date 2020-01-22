bc7a498a5871cca8bc685b70dc35fe768df7350e
daviskooo <daviferreiradelima.contato@gmail.com>
2020-01-22 01:03:09 -0300

    Criando um modelo de item, basicamente, uma classe que será reutilizada com seus métodos para criar os itens da lista. Primeiro método de recebimento em JSON criado.

---
+++ todo/lib/models/item.dart
@@ -0,0 +1,11 @@
+class Item {
+  String title;
+  bool done;
+
+  Item({this.title, this.done});
+
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+}
